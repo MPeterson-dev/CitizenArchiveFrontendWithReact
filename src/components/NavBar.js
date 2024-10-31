e35782b27d5a2f370aa64581b0d64742b9356470
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2024-10-31 14:30:19 -0700

    Initial commit, clean up and added NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return ( 
+        <nav>
+            <h1>Citizen Archive</h1>
+            {/* Nav elements here */}
+        </nav>
+    );
+};
+
+export default NavBar;
