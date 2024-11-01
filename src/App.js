b67ab7be43bbd22a6975aba0280248f1bb220b6a
MPeterson-dev <MPeterson.Developer@outlook.com>
2024-10-31 17:52:40 -0700

    Added routing, SubjectCards, and Topics Page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './App.css';
+import SubjectCard from './components/SubjectCard';
+import TopicPage from './components/TopicPage';
+
+const subjects = [
+    { icon: '🔬', title: 'Science', topicCount: 15 },
+    { icon: '📐', title: 'Math', topicCount: 10 },
+    { icon: '📜', title: 'History', topicCount: 8 },
+]
 
 const App = () => {
+    const navigate = useNavigate();
+
+    const handleCardClick = (title) => {
+        // Navigate to subjects topic page
+        navigate(`/topics/${title}`);
+    };
+
     return (
-        <div>
-            <NavBar />
-            <main className='container mt-4'>
-                <h1>Home page</h1>
-            </main>
-        </div>
+        
+            <div>
+                <NavBar />
+                <main className='container mt-4'>
+                    <Routes>
+                        {/* Home page with Subject Cards */}
+                        <Route
+                            path='/'
+                            element={
+                                <div className='d-flex flex-wrap justify-content-center'>
+                                    {subjects.map((subject, index) => (
+                                        <SubjectCard
+                                        key={index}
+                                        icon={subject.icon}
+                                        title={subject.title}
+                                        topicCount={subject.topicCount}
+                                        onClick={() => handleCardClick(subject.title)}
+                                        />
+                                    ))}
+                                </div>
+                            }
+                        />
+                        {/* Dynamic Topic Page Route e.g. click Science card /topics/Science  */}
+                        <Route path='/topics/:subject' element={<TopicPage />} />    
+                    </Routes>
+                </main>            
+            </div>
+        
     );
 };
 
---
+++ src/components/TopicPage.js
@@ -0,0 +1,16 @@
+// src/components/TopicPage.js
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+const TopicPage = () => {
+    const { subject } = useParams(); // Get subject title from URL
+
+    return (
+        <div>
+            <h2>{subject} Topics</h2>
+            
+        </div>
+    )
+}
+
+export default TopicPage;
