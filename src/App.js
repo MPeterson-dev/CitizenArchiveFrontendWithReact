2f3e13e4c2c35d2633fcbfffaf825334256ff3fc
MPeterson-dev <MPeterson.Developer@outlook.com>
2024-11-04 15:31:22 -0700

    Search bar dynamically updates. Subject cards placed in Bootstrap grid.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './App.css';
@@ -9,45 +9,56 @@ const subjects = [
     { icon: '🔬', title: 'Science', topicCount: 15 },
     { icon: '📐', title: 'Math', topicCount: 10 },
     { icon: '📜', title: 'History', topicCount: 8 },
+    //Will create list in database later. Only admin be able to add subjects. 
 ]
 
 const App = () => {
+    // useNavigate for Subject card click, nav to Topics page of that Subject.
     const navigate = useNavigate();
-
+    
     const handleCardClick = (title) => {
         // Navigate to subjects topic page
         navigate(`/topics/${title}`);
     };
+    
+    // Filter subjects based on user input in the search bar
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredSubjects = subjects.filter(subject =>
+            subject.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+
 
     return (
-        
-            <div>
-                <NavBar />
-                <main className='container mt-4'>
-                    <Routes>
-                        {/* Home page with Subject Cards */}
-                        <Route
-                            path='/'
-                            element={
-                                <div className='d-flex flex-wrap justify-content-center'>
-                                    {subjects.map((subject, index) => (
+        <div>
+            {/* Set searchTerm state to capture user input in search bar. */}
+            <NavBar setSearchTerm={setSearchTerm} />
+            <main className='container mt-4'>
+                <Routes>
+                    {/* Home page with Subject Cards */}
+                    <Route
+                        path='/'
+                        element={
+                            <div className='row justify-content-center'>
+                                {/* Filters subjects based on user input in search bar */}
+                                {filteredSubjects.map((subject, index) => (
+                                    <div key={index} className='col-6 col-md-4 col-lg-3 col-xl-2 mb-4'>
                                         <SubjectCard
-                                        key={index}
-                                        icon={subject.icon}
-                                        title={subject.title}
-                                        topicCount={subject.topicCount}
-                                        onClick={() => handleCardClick(subject.title)}
+                                            icon={subject.icon}
+                                            title={subject.title}
+                                            topicCount={subject.topicCount}
+                                            onClick={() => handleCardClick(subject.title)}
                                         />
-                                    ))}
-                                </div>
-                            }
-                        />
-                        {/* Dynamic Topic Page Route e.g. click Science card /topics/Science  */}
-                        <Route path='/topics/:subject' element={<TopicPage />} />    
-                    </Routes>
-                </main>            
-            </div>
-        
+                                    </div>
+                                ))}
+                            </div>
+                        }
+                    />
+                    {/* Dynamic Topic Page Route, e.g., clicking Science card goes to /topics/Science */}
+                    <Route path='/topics/:subject' element={<TopicPage />} />
+                </Routes>
+            </main>
+        </div>
     );
 };
 
